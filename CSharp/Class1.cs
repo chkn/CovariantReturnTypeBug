401e0321bfe55b892893d34e12310a6f46ac333a
Alex Corrado <alexander.corrado@gmail.com>
2022-05-26 22:29:02 -0400

    Add code

---
+++ CSharp/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharp;
+
+public abstract class Class1
+{
+	public abstract object Foo ();
+}
+
+public class Class2 : Class1
+{
+	public override string Foo ()
+	{
+		return "HELLO!";
+	}
+}
---
+++ FSharp/Program.fs
@@ -0,0 +1,7 @@
+﻿open CSharp
+
+type Class3() =
+    inherit Class2()
+
+printfn "%s" (Class3().Foo())
+
